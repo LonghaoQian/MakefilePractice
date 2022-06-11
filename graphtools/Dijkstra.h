0b59163743f7a409188a2a72f1bb76eb6ee31c9a
LonghaoQian <longhao.qian@mail.utoronto.ca>
2022-06-11 17:43:40 +0800

    add dijkstra

--- graphtools/Dijkstra.h
+++ graphtools/Dijkstra.h
@@ -1,6 +1,7 @@
 #ifndef DIJKSTRA_H
 #define DIJKSTRA_H
 #include <queue>
+#include <limits>
 #include "GraphCommon.h"
 namespace GraphAnalysis {
     using Path = std::vector<NodeIndex>;
@@ -11,8 +12,17 @@ namespace GraphAnalysis {
                  const NodeCostXd& nodeCost,
                  unsigned int numOfNodes);
         virtual ~Dijkstra();
-        std::vector<Path> GetResult();
+        void Reset(const AdjacencyXi& adjacencyMatrix, 
+                   const PathCostXd& pathCost,
+                   const NodeCostXd& nodeCost,
+                   unsigned int numOfNodes);
+        std::pair<std::vector<Path>, double> GetResult(NodeIndex destination);
+        void PrintConnectionMatrix();
+        void PrintNodeConnectionList();
     private:
+        bool CheckAdjacencyMatrix();
+        void BfsTraverseTree(NodeIndex root);
+        void RecordResult();
         AdjacencyXi adjacency;
         std::priority_queue<double> buffer;
         std::vector<Node> nodeList;
