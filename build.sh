a79d541aa6712f78557727c51f39259e70204edf
longhao qian <longhao.qian@maill.utoronto.ca>
2022-08-06 13:46:00 +0800

    refractored directories

--- build.sh
+++ build.sh
@@ -1,1 +1,8 @@
+if [ -d "build" ]
+then
+    echo "remove build directory."
+    rm -r build
+fi
+mkdir build
+cmake -S . -B build -DCMAKE_BUILD_TYPE=Debug
 cmake --build build
---
+++ desgin_mode_test/module_register.cpp
@@ -0,0 +1,38 @@
+#include "module_register.h"
+#include <iostream>
+
+namespace ModelBuilder
+{
+BlockBuilderBase::BlockBuilderBase() {}
+
+BlockBuilderBase::~BlockBuilderBase() {}
+
+BlockResiger::BlockResiger()
+{
+    // registor function table
+    std::cout << "resgister constructor is called. \n";
+}
+BlockResiger::~BlockResiger()
+{
+    for (auto it = funcList.begin(); it != funcList.end(); it++) {
+        delete it->second;
+    }
+}
+BlockResiger &BlockResiger::CheckFuncList()
+{
+    static BlockResiger br;
+    return br;
+}
+void BlockResiger::CallFuncList(int idx)
+{
+    if (funcList.empty()) {
+        std::cout << "no method to call! \n";
+        return;
+    }
+    funcList[idx]->AddBlock();
+}
+void BlockResiger::AddToFuncList(int idx, BlockBuilderBase *func)
+{
+    funcList[idx] = func;
+}
+}  // namespace ModelBuilder
