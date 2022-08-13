e3528d8b35b8bb219fc82e58a5bed38f9cd15c55
longhao qian <longhao.qian@maill.utoronto.ca>
2022-08-13 09:52:11 +0800

    add address sanitizer

--- config.sh
+++ config.sh
@@ -1,8 +1,1 @@
-if [ -d "build" ]
-then
-    echo "remove build directory."
-    rm -r build
-fi
-mkdir build
-# TO DO: add option to detect ninjia generator
 cmake -S . -B build -DCMAKE_BUILD_TYPE=Debug
--- src/singleton_test.cpp
+++ src/singleton_test.cpp
@@ -1,6 +1,7 @@
 #include "module_register.h"
 #include "test_module_0.h"
-
+#include <vector>
+#include <iostream>
 using namespace ModelBuilder;
 
 int main(void)
